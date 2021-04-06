a0494f3defbc28e24f8611ce60474f4b44c53ad5
Contributor 2 <contributor2@mail.com>
2021-04-06 19:08:02 +0200

    Added org.json as an requirement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module project.toDoListApp {
   requires javafx.controls;
   requires java.logging;
+  requires org.json;
   exports project.toDoListApp.view;
 
   opens project.toDoListApp to javafx.base;
