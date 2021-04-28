feb187140efdab13ff224735f306a473bee56fec
Contributor 2 <contributor2@mail.com>
2021-04-28 15:06:56 +0200

    commented out test values

--- src/main/java/project/toDoListApp/controller/Controller.java
+++ src/main/java/project/toDoListApp/controller/Controller.java
@@ -131,7 +131,7 @@ public class Controller {
    * Adds a few tasks to the register for testing.
    */
   private void fillRegisterWithTestTasks() {
-    this.taskRegister.addTask(new Task("Title 1", "Desc 1",
+    /*this.taskRegister.addTask(new Task("Title 1", "Desc 1",
         "None", LocalDate.parse("2100-12-01")));
     this.taskRegister.addTask(new Task("Title 2", "Desc 2",
         "Cooking", LocalDate.parse("3100-12-01")));
@@ -141,6 +141,8 @@ public class Controller {
             .build());
 
     this.updateObservableList();
+
+     */
   }
 
   /**
@@ -314,15 +316,22 @@ public class Controller {
     if (result.isPresent()) {
       if (result.get() == ButtonType.OK) {
         this.saveTaskToRegister(taskTitle, editor);
-        try {
-          fileUtility.saveToFile("tasks/savedTasks.txt",taskRegister);
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
+        saveTaskRegisterToFile();
         Platform.exit();
       } else {
         event.consume();
       }
+    }
+  }
+
+  /**
+   *  Saves the current tasks in the task register to a file.
+   */
+  public void saveTaskRegisterToFile(){
+    try {
+      fileUtility.saveToFile("tasks/savedTasks.txt",taskRegister);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
